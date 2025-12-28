723b7184d01804e6adae94eeb08eca493de63a8e
IvanPustovit <pustovit.for@gmail.com>
2025-12-28 19:30:42 +0200

    up finall

--- app/final/page.tsx
+++ app/final/page.tsx
@@ -1,11 +1,24 @@
+'use client';
+
 import Link from "next/link";
 import VideoBackground from "@/components/VideoBackground";
 import AudioPlayer from "@/components/AudioPlayer";
+import { useEffect, useState } from "react";
+
 export default function FinalPage() {
+    const [videoSrc, setVideoSrc] = useState("/videos/final1.mp4");
+
+    useEffect(() => {
+        const interval = setTimeout(() => {
+            setVideoSrc(prev => prev === "/videos/final1.mp4" ? "/videos/final2.mp4" : "/videos/final1.mp4");
+        }, 40000);
+
+        return () => clearTimeout(interval);
+    }, []);
     return (
         <main>
-            <AudioPlayer src="/music/santa2.mp3"  />
-            <VideoBackground src="/videos/final1.mp4" />
+            <AudioPlayer src="/music/santa2.mp3" />
+            <VideoBackground src={videoSrc} />
             <div className="page-container">
                 {/* <h1 className="title">З Новим Роком!</h1> */}
                 {/* <p style={{ fontSize: '1.5rem', marginBottom: '2rem', maxWidth: '600px', lineHeight: '1.6' }}>
